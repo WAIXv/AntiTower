aa462507462f417330610f19e119933e44ca4204
WAI <why18520259443@gmail.com>
2023-05-02 18:47:01 +0800

    feat-角色受击，地块switch

--- Assets/Scripts/ActorMono/SwapNodeCube.cs
+++ Assets/Scripts/ActorMono/SwapNodeCube.cs
@@ -1,23 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 
 public class SwapNodeCube : PathNodeCube
 {
-    [Header("Swap Node")]
-    [SerializeField] private PathNodeCube nextCube_on;
-    [SerializeField] private PathNodeCube nextCube_off;
+    [Header("Swap Node")] 
+    [SerializeField] private List<PathNodeCube> nextCubeList;
 
-    [SerializeField] private bool toggle;
-
-    public void ChangeNextCube()
-    {
-        toggle = !toggle;
-        NextCube = toggle ? nextCube_on : nextCube_off;
-    }
+    private int _curIndex = 0;
+    
+#if UNITY_EDITOR
+    [SerializeField] public bool toggle;
+#endif
 
     private void OnEnable()
     {
-        ChangeNextCube();
+        NextCube = nextCubeList[_curIndex];
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (toggle)
+        {
+            ChangeNextCube();
+            toggle = false;
+        }
+#endif
+    }
+
+    public void ChangeNextCube()
+    {
+        _curIndex++;
+        if(_curIndex == nextCubeList.Count) _curIndex = 0;
+        NextCube = nextCubeList[_curIndex];
     }
 }
---
+++ Assets/Scripts/Charactor/StateMachine/Actions/ClearMovementVectorActionSO.cs
@@ -0,0 +1,41 @@
+﻿using Charactor;
+using UnityEngine;
+using UOP1.StateMachine;
+using UOP1.StateMachine.ScriptableObjects;
+
+[CreateAssetMenu(fileName = "ClearMovementVectorAction", menuName = "State Machines/Actions/Clear Movement Vector Action")]
+public class ClearMovementVectorActionSO : StateActionSO
+{
+	public StateAction.SpecificMoment _moment;
+	protected override StateAction CreateAction() => new ClearMovementVectorAction();
+}
+
+public class ClearMovementVectorAction : StateAction
+{
+	private CharactorBrain _charactor;
+	private SpecificMoment _moment;
+	private ClearMovementVectorActionSO _originSO => OriginSO as ClearMovementVectorActionSO;
+	public override void Awake(UOP1.StateMachine.StateMachine stateMachine)
+	{
+		
+		_charactor = stateMachine.GetComponent<CharactorBrain>();
+	}
+	
+	public override void OnUpdate()
+	{
+		if(_moment == SpecificMoment.OnUpdate)
+			_charactor.movementVector = Vector3.zero;
+	}
+	
+	public override void OnStateEnter()
+	{
+		if(_moment == SpecificMoment.OnStateEnter)
+			_charactor.movementVector = Vector3.zero;
+	}
+	
+	public override void OnStateExit()
+	{
+		if(_moment == SpecificMoment.OnStateExit)
+			_charactor.movementVector = Vector3.zero;
+	}
+}
